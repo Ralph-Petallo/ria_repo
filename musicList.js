6767e57330818e3f2e7d6b0a46344c94d8c71fee
ralphpetallo <ralphpetallo@gmail.com>
2026-08-27 16:13:00 +0800

    update: new songs added

--- musicList.js
+++ musicList.js
@@ -1,5 +1,12 @@
 const tracks = [{
     title:
+        "Happy Birthday To You",
+    artist:
+        "THE KIBOOMERS",
+    src:
+        "music/Happy Birthday To You - THE KIBOOMERS.mp3"
+}, {
+    title:
         "Pokémon D/P/P",
     artist:
         "Jubilife City",
@@ -15,34 +22,32 @@ const tracks = [{
 }, {
     title:
         "Thinking of You",
-
     artist:
         "Katy Perry",
-
     src:
         "music/Katy Perry - Thinking of you (Lyrics).mp3"
-},
-{
+}, {
     title:
         "Your Guardian Angel",
-
     artist:
         "The Red Jumpsuit Apparataus",
-
     src:
         "music/The Red Jumpsuit Apparatus - Your Guardian Angel (Lyrics).mp3"
-},
-{
+}, {
     title:
         "Kawaki wo Ameku",
-
     artist:
         "Minami",
-
     src:
         "music/Minami - Kawaki wo Ameku Lyrics (Crying for Rain)_Domestic na Kanojo Op song [rse6a3MSKto].mp3"
-}
-];
+}, {
+    title:
+        "Huling Sandali",
+    artist:
+        "December Avenue",
+    src:
+        "music/December Avenue - Huling Sandali (OFFICIAL LYRIC VIDEO).mp3"
+}];
 
 export default function MusicList() {
     return tracks

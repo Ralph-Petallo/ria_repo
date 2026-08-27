5c32cab56705343ee273ef2028fd9c8c12248cf1
ralphpetallo <ralphpetallo@gmail.com>
2026-08-27 15:44:05 +0800

    added new songs

---
+++ musicList.js
@@ -0,0 +1,49 @@
+const tracks = [{
+    title:
+        "Pokémon D/P/P",
+    artist:
+        "Jubilife City",
+    src:
+        "/music/Pokémon D_P_P - Jubilife City.mp3"
+}, {
+    title:
+        "Moment Of Truth",
+    artist:
+        "FM Static",
+    src:
+        "/music/Moment Of Truth - FM Static (Lyrics).mp3"
+}, {
+    title:
+        "Thinking of You",
+
+    artist:
+        "Katy Perry",
+
+    src:
+        "music/Katy Perry - Thinking of you (Lyrics).mp3"
+},
+{
+    title:
+        "Your Guardian Angel",
+
+    artist:
+        "The Red Jumpsuit Apparataus",
+
+    src:
+        "music/The Red Jumpsuit Apparatus - Your Guardian Angel (Lyrics).mp3"
+},
+{
+    title:
+        "Kawaki wo Ameku",
+
+    artist:
+        "Minami",
+
+    src:
+        "music/Minami - Kawaki wo Ameku Lyrics (Crying for Rain)_Domestic na Kanojo Op song [rse6a3MSKto].mp3"
+}
+];
+
+export default function MusicList() {
+    return tracks
+}
